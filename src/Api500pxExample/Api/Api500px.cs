cae003b37c6c4b59af59795b5c48c2862533a7cf
Vasileios Samaltanos <vas@ozschooltek.com.au>
2016-02-26 19:52:22 +1100

    Finished RequestToken()

--- src/Api500pxExample/Api/Api500px.cs
+++ src/Api500pxExample/Api/Api500px.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Net.Http;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Api500pxExample.Api.Models;
 
@@ -16,7 +20,11 @@ namespace Api500pxExample.Api
     /// </summary>
     public class Api500px //: IApi500px
     {
-        private readonly Uri _baseAddress = new Uri("https://api.500px.com/v1");
+        private const string AccessUrl = "https://api.500px.com/v1/oauth/access_token";
+        private const string AuthorizeUrl = "https://api.500px.com/v1/oauth/authorize";
+        private const string RequestTokenUrl = "https://api.500px.com/v1/oauth/request_token";
+        private const string OAuthSignatureMethod = "HMAC-SHA1";
+        private const string OAuthVersion = "1.0";
         private const string CallbackUrl = "http://www.google.com";
         private Dictionary<string, string> AuthorizationParameters;
 
@@ -45,8 +53,8 @@ namespace Api500pxExample.Api
         {
             try
             {
-                await RequestToken();
-                await AuthorizeToken();
+                var token = await RequestToken();
+                await AuthorizeToken(token);
                 await AccessToken();
                 return true;
             }
@@ -58,52 +66,67 @@ namespace Api500pxExample.Api
 
         private async Task<OauthToken> RequestToken()
         {
+            var token = new OauthToken();
 
             AuthorizationParameters = new Dictionary<string, string>()
                                                                      {
                                                                          {OauthParameter.OauthCallback, CallbackUrl},
-                                                                         {OauthParameter.OauthConsumerKey, ConsumerKey},
+                                                                         {OauthParameter.OauthConsumerKey, Constants.ConsumerKey},
                                                                          {OauthParameter.OauthNonce, GetNonce()},
-                                                                         {OauthParameter.OauthSignatureMethod,OAuthSignatureMethod},
+                                                                         {OauthParameter.OauthSignatureMethod, OAuthSignatureMethod},
                                                                          {OauthParameter.OauthTimestamp, GetTimeStamp()},
                                                                          {OauthParameter.OauthVersion, OAuthVersion}
                                                                      };
 
-            var signature = GetSignature(sigBaseString);
+            var response = await Sign(RequestTokenUrl, string.Empty, "POST").PostRequest(RequestTokenUrl);
 
-            var resourceUri = RequestTokenUrl + "?" + sigBaseStringParams + "&oauth_signature=" + Uri.EscapeDataString(signature);
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetStringAsync(new Uri(resourceUri));
-
-            string requestToken = null;
-            var keyValPairs = response.Split('&');
-
-            foreach (var t in keyValPairs)
+            if (!string.IsNullOrEmpty(response))
             {
-                var splits = t.Split('=');
-                switch (splits[0])
+                //  "oauth_token=qWnfRDBG47T57ud3peQeYtWHjoGSlbYtAwboQZuD&oauth_token_secret=iZJG3SKMA8BJexk4iEtQTwZmbstfR1TqoY1yKBo6&oauth_callback_confirmed=true"    string
+                var keyValPairs = response.Split('&');
+                foreach (var splits in keyValPairs.Select(t => t.Split('=')))
                 {
-                    case "oauth_token":
-                        requestToken = splits[1];
-                        break;
-                    case "oauth_token_secret":
-                        break;
+                    switch (splits[0])
+                    {
+                        case "oauth_token":
+                            token.Token = splits[1];
+                            break;
+                        case "oauth_token_secret":
+                            token.SecretCode = splits[1];
+                            break;
+                    }
                 }
             }
 
-            return requestToken;
+            return token;
         }
 
-        private async Task<OauthToken> AuthorizeToken()
+        private async Task<OauthToken> AuthorizeToken(OauthToken token)
         {
+            var tempAuthorizeUrl = AuthorizeUrl + "?oauth_token=" + token.Token;
 
+            var StartUri = new Uri(tempAuthorizeUrl);
+            var EndUri = new Uri(CallbackUrl);
+
+            var auth =
+                await
+                WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, StartUri, EndUri);
+
+            //var responseData = auth.ResponseData;
+            //responseData = responseData.Replace("http://www.bing.com/?", "");
+            //var split = responseData.Split('&');
+            //var keyValue = split[1].Split('=');
+            //Token.Verifier = keyValue[1];
+            //return Token;
+
+            return null;
         }
 
         private async Task<OauthToken> AccessToken()
         {
-
+            return null;
         }
- 
+
         private static string GetNonce()
         {
             var rand = new Random();
@@ -117,20 +140,61 @@ namespace Api500pxExample.Api
             return Math.Round(sinceEpoch.TotalSeconds).ToString(CultureInfo.InvariantCulture);
         }
 
-        private string GetSignature(string Url, string tokenSecret, string requestType)
+        private Api500px Sign(string uri, string tokenSecret, string requestType)
         {
-            var SigBaseStringParams = string.Join("&", AuthorizationParameters.Select(key => key.Key + "=" + Uri.EscapeDataString(key.Value)));
-            var SigBaseString = requestType + "&";
-            SigBaseString += Uri.EscapeDataString(Url) + "&" + Uri.EscapeDataString(SigBaseStringParams);
+            var signatureParams = string.Join("&", AuthorizationParameters.Select(key => key.Key + "=" + Uri.EscapeDataString(key.Value)));
+            var signatureBase = requestType + "&";
+            signatureBase += Uri.EscapeDataString(uri) + "&" + Uri.EscapeDataString(signatureParams);
 
-            IBuffer KeyMaterial = CryptographicBuffer.ConvertStringToBinary(Constants.ConsumerSecret + "&" + tokenSecret, BinaryStringEncoding.Utf8);
-            MacAlgorithmProvider HmacSha1Provider = MacAlgorithmProvider.OpenAlgorithm(OAuthSignatureMethodName);
-            CryptographicKey MacKey = HmacSha1Provider.CreateKey(KeyMaterial);
-            IBuffer DataToBeSigned = CryptographicBuffer.ConvertStringToBinary(SigBaseString, BinaryStringEncoding.Utf8);
-            IBuffer SignatureBuffer = CryptographicEngine.Sign(MacKey, DataToBeSigned);
-            String Signature = CryptographicBuffer.EncodeToBase64String(SignatureBuffer);
-            AuthorizationParameters.Add(OauthParameter.OauthSignature, Signature);
+            var hash = GetHash(tokenSecret);
+            var dataBuffer = System.Text.Encoding.ASCII.GetBytes(signatureBase);
+            var hashBytes = hash.ComputeHash(dataBuffer);
+
+            AuthorizationParameters.Add(OauthParameter.OauthSignature, Convert.ToBase64String(hashBytes));
             return this;
+        }
+
+        private HashAlgorithm GetHash(string tokenSecret)
+        {
+            if (OAuthSignatureMethod != "HMAC-SHA1") throw new NotImplementedException();
+
+            var keystring = string.Format("{0}&{1}", Constants.ConsumerSecret, tokenSecret);
+
+            var hmacsha1 = new HMACSHA1
+            {
+                Key = System.Text.Encoding.ASCII.GetBytes(keystring)
+            };
+            return hmacsha1;
+        }
+
+        private async Task<string> PostRequest(string url)
+        {
+            var oauthString = string.Empty;
+            if (AuthorizationParameters != null)
+            {
+                oauthString = string.Join(", ",
+                                               AuthorizationParameters.Select(
+                                                   key =>
+                                                   key.Key +
+                                                   (string.IsNullOrEmpty(key.Value)
+                                                        ? string.Empty
+                                                        : "=\"" + Uri.EscapeDataString(key.Value) + "\"")));
+                AuthorizationParameters.Clear();
+            }
+
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("OAuth", oauthString);
+                var response = await client.PostAsync(new Uri(url, UriKind.Absolute), null);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+            }
+
+            return string.Empty;
         }
     }
 }
